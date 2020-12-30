e31701cb42fdbf21c3a6b1d79979dd7445eda79f
Tom Cuillandre <tom.cuillandre@sfr.fr>
2020-12-30 15:17:30 +0100

    ADD responsive design, dark/light mod

---
+++ public/js/selectedCategory.js
@@ -0,0 +1,7 @@
+var categories = document.getElementById("navbar").getElementsByClassName("navbtn");
+
+for (var i = 0; i < categories.length; i++) {
+    if(categories[i].id == document.location.pathname){
+        categories[i].className = "selected";
+    }
+}
---
+++ public/js/themeSwitcher.js
@@ -0,0 +1,31 @@
+var themeSwitcher = document.getElementById('theme-switcher');
+
+if (themeSwitcher) {
+    if (document.body.classList.contains('dark-theme')) {
+        themeSwitcher.innerText = 'Switch to light theme';
+    }
+
+    themeSwitcher.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        if (document.body.classList.contains('dark-theme')) {
+            document.body.classList.remove('dark-theme');
+            document.body.classList.add('light-theme');
+            localStorage.setItem('themeName', 'light-theme');
+            themeSwitcher.innerText = 'Switch to dark theme';
+        } else if (document.body.classList.contains('light-theme')) {
+            document.body.classList.remove('light-theme');
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('themeName', 'dark-theme');
+            themeSwitcher.innerText = 'Switch to light theme';
+        }
+    });
+}
+
+document.body.classList.add(
+    localStorage.getItem('themeName') || (matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme')
+);
+
+document.body.classList.add(
+    (navigator.appVersion.indexOf('Win') !== -1) ? 'windows' : (navigator.appVersion.indexOf('Mac') !== -1) ? 'macos' : 'linux'
+);
